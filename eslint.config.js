7161596e83b24188af2a6c242c604f505b8007bd
zhangzhewen <nsync_zzw@163.com>
2026-07-13 07:42:04 +0800

    chore: 初始化前端工作区

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import vueParser from 'vue-eslint-parser'
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/.claude/**',
+      '**/.codex/**',
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/coverage/**',
+      '**/playwright-report/**',
+      '**/test-results/**',
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
+  },
+)
